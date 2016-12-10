219b2d822d4ad558d4ae03cc35c1461e8d2d8a46
dkdoes <david.kennedy.does@gmail.com>
2016-12-09 19:45:30 -0800

    ...

--- game.js
+++ game.js
@@ -13,8 +13,9 @@ window.onload = function(){
     camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000)
     camera.position.set(0,10,10)
     camera.update = function(){
-        camera.rotation.lookAt(player.mesh.position)
+        camera.lookAt(player.position)
     }
+    scene.add(camera)
     resize = function(e){
         e.preventDefault()
         renderer.setSize(window.innerWidth, window.innerHeight)
@@ -26,7 +27,20 @@ window.onload = function(){
     }
     window.addEventListener('resize',resize)
     window.dispatchEvent(new Event('resize'))
-    
+    player = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshLambertMaterial({color:0xdddddd})
+    )
+    scene.add(player)
+    skyLight = new THREE.DirectionalLight(0xffffff, 1)
+    skyLight.position.set(1,2,1)
+    scene.add(skyLight)
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+    scene.add(ambientLight)
+    floorLight = new THREE.DirectionalLight(0xffffff, 0.1)
+    floorLight.position.set(0,-1,0)
+    scene.add(floorLight)
+    render()
 }
 
 render = function(){
@@ -34,9 +48,9 @@ render = function(){
     delta = clock.getDelta()
     TWEEN.update()
     world.step(1/60,delta,10)
-    for(o=0;o<scene.children.length;o++){
+    for(i=0;i<scene.children.length;i++){
         try{
-            o.update()
+            scene.children[i].update()
         }catch(err){}
     }
     renderer.render(scene,camera)
